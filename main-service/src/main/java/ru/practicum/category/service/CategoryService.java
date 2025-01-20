89eaab6addc4589c0f91c6cdecc6f8f69ea36784
IvchenkoEV <pesok72@mail.ru>
2025-01-20 09:44:47 +0500

    #2 Public: Категории

--- main-service/src/main/java/ru/practicum/category/service/CategoryService.java
+++ main-service/src/main/java/ru/practicum/category/service/CategoryService.java
@@ -3,10 +3,16 @@ package ru.practicum.category.service;
 import ru.practicum.category.dto.CategoryDto;
 import ru.practicum.category.dto.NewCategoryDto;
 
+import java.util.List;
+
 public interface CategoryService {
     CategoryDto saveCategory(NewCategoryDto dto);
 
     void deleteCategory(Long catId);
 
     CategoryDto updateCategory(Long catId, NewCategoryDto dto);
+
+    List<CategoryDto> getCategories(int from, int size);
+
+    CategoryDto getCategoryById(Long catId);
 }
--- main-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
+++ main-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.category.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.category.dto.CategoryDto;
@@ -9,6 +10,8 @@ import ru.practicum.category.mapper.CategoryMapper;
 import ru.practicum.category.model.Category;
 import ru.practicum.category.repository.CategoryRepository;
 import ru.practicum.common.exception.NotFoundException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -35,11 +38,27 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Transactional
     public CategoryDto updateCategory(Long catId, NewCategoryDto dto) {
         Category categoryToUpdate = categoryRepository.findById(catId)
                 .orElseThrow(() -> new NotFoundException("Category with id=" + catId + " was not found"));
         categoryToUpdate.setName(dto.getName());
         categoryRepository.save(categoryToUpdate); //неуникальное имя тут само будет обработано базой
         return mapper.toDto(categoryToUpdate);
+    }
+
+    @Override
+    public List<CategoryDto> getCategories(int from, int size) {
+        PageRequest page = PageRequest.of(from, size);
+        return categoryRepository.findAll(page).stream()
+                .map(mapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public CategoryDto getCategoryById(Long catId) {
+        Category category = categoryRepository.findById(catId)
+                .orElseThrow(() -> new NotFoundException("Category with id=" + catId + " was not found"));
+        return mapper.toDto(category);
     }
 }
