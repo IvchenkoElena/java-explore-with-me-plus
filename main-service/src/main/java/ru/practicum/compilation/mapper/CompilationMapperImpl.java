a266bed7fe0f206571e583344a696fe87849979e
iolinpic <iolinpic@gmail.com>
2025-01-24 22:52:44 +0300

    delete unused calls to views and requests in EventShortDto

--- main-service/src/main/java/ru/practicum/compilation/mapper/CompilationMapperImpl.java
+++ main-service/src/main/java/ru/practicum/compilation/mapper/CompilationMapperImpl.java
@@ -4,18 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.compilation.dto.CompilationDto;
 import ru.practicum.compilation.model.Compilation;
+import ru.practicum.events.mapper.EventMapper;
 
 @RequiredArgsConstructor
 @Component
 public final class CompilationMapperImpl {
-    private final CompilationMapperUtil compilationMapperUtil;
+    private final EventMapper eventMapper;
 
     public CompilationDto toDto(Compilation compilation) {
         CompilationDto dto = new CompilationDto();
         dto.setId(compilation.getId());
         dto.setPinned(compilation.isPinned());
         dto.setTitle(compilation.getTitle());
-        dto.setEvents(compilationMapperUtil.convert(compilation.getEvents()));
+        dto.setEvents(compilation.getEvents().stream().map(eventMapper::toEventShortDto).toList());
         return dto;
     }
 
